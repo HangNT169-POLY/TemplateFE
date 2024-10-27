ee389ffc2ccf239573d71cce734dd445507b1ba6
HangNT169 <HangNT169@fpt.edu.vn>
2024-10-28 00:19:35 +0700

    update

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import FooterLayout from './layout/FooterLayout.vue'
+import HeaderLayout from './layout/HeaderLayout.vue'
+</script>
+
+<template>
+  <HeaderLayout />
+  <main>
+    <RouterView />
+  </main>
+  <FooterLayout />
+</template>
